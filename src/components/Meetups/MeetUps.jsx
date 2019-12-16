e16bcfd909b267b915fad8097a4d9b643ecffe2f
Cody Young <codyjamesyoung@gmail.com>
2019-12-16 12:33:13 -0700

    first commit

---
+++ src/components/Meetups/MeetUps.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MeetUps extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MeetUps;
---
+++ src/components/Startups/StartUps.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class StartUps extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default StartUps;
