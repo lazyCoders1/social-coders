e245d11173c3528d880b01e969524edf14c3940a
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-12-16 16:51:53 -0700

    routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    )
+  }
 }
 
-export default Header;
+export default Header
