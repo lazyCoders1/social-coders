bb9ed6dab8cfddf16d73d72f6173c38f8cf0e64f
jnjagod <josh.jagoda@gmail.com>
2019-12-18 15:34:54 -0800

    channel children updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,10 +16,26 @@ import {
 } from "mdbreact";
 import { BrowserRouter as Router } from "react-router-dom";
 import { connect } from "react-redux";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 export class Header extends Component {
   state = {
     isOpen: false
+  };
+
+  logout = () => {
+    axios
+      .delete("/auth/logout")
+      .then(res => {
+        Swal.fire({
+          title: res.data.message,
+          icon: "success",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      })
+      .catch(err => console.log(err));
   };
 
   toggleCollapse = () => {
@@ -29,10 +45,18 @@ export class Header extends Component {
   render() {
     return (
       <Router>
-        <MDBNavbar className='position-sticky' color="default-color" dark expand="md" style={{position: "fixed", top: 0, width: '100vw', zIndex: '10'}} >
+        <MDBNavbar
+          className="position-sticky"
+          color="default-color"
+          dark
+          expand="md"
+          style={{ position: "fixed", top: 0, width: "100vw", zIndex: "10" }}
+        >
           {/* <MDBNavbar color="grey lighten-5" dark expand="md"> */}
           <MDBNavbarBrand>
-            <strong className="white-text">Social Coders</strong>
+            <strong className="white-text" style={{ cursor: "pointer" }}>
+              Social Coders
+            </strong>
           </MDBNavbarBrand>
           <MDBNavbarToggler onClick={this.toggleCollapse} />
           <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
@@ -58,11 +82,17 @@ export class Header extends Component {
                       <div className="d-none d-md-inline">Channels</div>
                     </MDBDropdownToggle>
                     <MDBDropdownMenu right className="dropdown-default">
-                      <MDBDropdownItem href="#!">JavaScript</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">CSS</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Public</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Meetups</MDBDropdownItem>
-                      <MDBDropdownItem href="#!">Startups</MDBDropdownItem>
+                      <MDBDropdownItem href="#/javascript">
+                        JavaScript
+                      </MDBDropdownItem>
+                      <MDBDropdownItem href="#/css">CSS</MDBDropdownItem>
+                      <MDBDropdownItem href="#/public">Public</MDBDropdownItem>
+                      <MDBDropdownItem href="#/meetups">
+                        Meetups
+                      </MDBDropdownItem>
+                      <MDBDropdownItem href="#/startups">
+                        Startups
+                      </MDBDropdownItem>
                     </MDBDropdownMenu>
                   </MDBDropdown>
                 </MDBNavItem>
@@ -75,14 +105,20 @@ export class Header extends Component {
                         <MDBIcon icon="user" />
                       </MDBDropdownToggle>
                       <MDBDropdownMenu right className="dropdown-default">
-                        <MDBDropdownItem href="#!">Profile</MDBDropdownItem>
-                        <MDBDropdownItem href="#!">Favorites</MDBDropdownItem>
-                        <MDBDropdownItem href="#!">SignOut</MDBDropdownItem>
+                        <MDBDropdownItem href="#/profile">
+                          Profile
+                        </MDBDropdownItem>
+                        <MDBDropdownItem href="#/favorites">
+                          Favorites
+                        </MDBDropdownItem>
+                        <MDBDropdownItem onClick={this.logout} href="#/">
+                          SignOut
+                        </MDBDropdownItem>
                       </MDBDropdownMenu>
                     </MDBDropdown>
                   </MDBNavItem>
                 ) : (
-                  <div style={{display: 'flex'}}>
+                  <div style={{ display: "flex" }}>
                     <MDBBtn color="warning" size="sm">
                       SignUp
                     </MDBBtn>
