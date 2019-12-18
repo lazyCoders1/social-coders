f005be4c5848912a53cc461782edb25f3751c272
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-12-17 20:15:12 -0700

    dashUpdate

--- src/components/Meetups/MeetUps.jsx
+++ src/components/Meetups/MeetUps.jsx
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react'
 
 class MeetUps extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+  render() {
+    const mapStyles = {
+      width: '30vw',
+      height: '40vh',
+      margin: '2rem'
     }
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176 }}
+        >
+          <Marker
+            position={{
+              address: '1600+Amphitheatre+Parkway,+Mountain+View,+CA'
+            }}
+          />
+        </Map>
+      </div>
+    )
+  }
 }
 
-export default MeetUps;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBTEz9tmLc2NzmQ6Aq_zt9qE3OjWgoiGRM'
+})(MeetUps)
