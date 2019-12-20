931d4f94bcb9b0ce42fff60c19dc249a5f978f9f
Shokupanman <Harrisonkaidehancock@gmail.com>
2019-12-20 09:45:49 -0700

    merge

--- src/components/Meetups/PostMeetUp.js
+++ src/components/Meetups/PostMeetUp.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import { MDBInput, MDBAnimation, MDBBtn, MDBIcon } from 'mdbreact'
+import { MDBInput, MDBAnimation, MDBBtn, MDBIcon, MDBContainer } from 'mdbreact'
 
 export default function PostMeetUp(props) {
   const el = props.meetUpPosts
   return (
     <div>
-      <MDBInput value={el.title} />
-      <MDBInput value={el.img} />
-      <MDBInput value={el.date} />
-      <MDBInput value={el.time} />
-      <MDBInput value={el.description} />
-      <MDBInput value={el.street} />
-      <MDBInput value={el.city} />
-      <MDBInput value={el.state} />
-      <MDBInput value={el.zipcode} />
+      <MDBContainer>
+        <MDBInput value={el.title} />
+        <MDBInput value={el.img} />
+        <MDBInput value={el.date} />
+        <MDBInput value={el.time} />
+        <MDBInput value={el.description} />
+        <MDBInput value={el.street} />
+        <MDBInput value={el.city} />
+        <MDBInput value={el.state} />
+        <MDBInput value={el.zipcode} />
+      </MDBContainer>
     </div>
   )
 }
