e65b87d82b2c517771c505302c95b6562fd65659
jnjagod <josh.jagoda@gmail.com>
2019-12-20 09:12:12 -0800

    auth/me

--- src/components/Posts/Post.jsx
+++ src/components/Posts/Post.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import { MDBIcon } from "mdbreact";
 import "./post.scss";
-import axios from 'axios'
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import parse from 'html-react-parser'
 
 class Post extends Component {
-    
-    addFavorite = () => {
-        let {id, title, img, content, author_id, category} = this.props.post
-        let {user_id, name, profile_pic} = this.props.profile
-        let user = {user_id, name, profile_pic}
-        // console.log(user)
-        let post = {id, title, img, content, author_id, category}
-        let favoritePost = {...user, ...post}
-        // console.log(favoritePost)
-            axios.post(`/api/favorites/${this.props.post.id}`, favoritePost)
-            .then(toast("Post added to favorites!"))
-    }
-    
+  addFavorite = () => {
+    let { id, title, img, content, author_id, category } = this.props.post;
+    let { user_id, name, profile_pic } = this.props.profile;
+    let user = { user_id, name, profile_pic };
+    // console.log(user)
+    let post = { id, title, img, content, author_id, category };
+    let favoritePost = { ...user, ...post };
+    // console.log(favoritePost)
+    axios
+      .post(`/api/favorites/${this.props.post.id}`, favoritePost)
+      .then(toast("Post added to favorites!"));
+  };
+
   render() {
     const post = this.props.post;
     const user = this.props.profile;
@@ -39,9 +40,9 @@ class Post extends Component {
         />
         <h2 className="users-name">{user.name}</h2>
         <h4 className="time">12 hrs</h4>
-  <h4 className='title'>{post.title}</h4>
-        <div className="post-content">{post.content}</div>
-        <img className="post-picture" src={`${post.img}`} alt='' />
+        <h4 className="title">{post.title}</h4>
+        <div className="post-content">{parse(post.content)}</div>
+        <img className="post-picture" src={`${post.img}`} alt="" />
         <h5 className="likes">43 likes</h5>
         <div className="icons">
           <div className="icon-box">
